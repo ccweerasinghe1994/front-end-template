a2d5016ba31b496fc6f9baa339114755b94140fa
ccweerasinghe1994 <chamara.weerasinghe@my.sliit.lk>
2023-09-10 13:14:35 +0530

    sample header

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,17 @@ import './App.css';
 
 function App() {
 	return (
-		<div className={'bg-slate-500 h-[100vh]'}>
-			<h1 className="text-3xl underline text-center pt-10">
-				this is a template
-			</h1>
+		<div
+			className={
+				'flex justify-between text-2xl pt-3 px-3 text-gray-600 pb-5 bg-amber-100'
+			}>
+			<h1 className={'font-primary'}>App Name</h1>
+			<ul className={'flex gap-5 text-2xl font-secondary font-bold'}>
+				<li className={'border-2 border-amber-300 rounded px-5'}>Home</li>
+				<li className={'border-2 border-amber-300 rounded px-5'}>About</li>
+				<li className={'border-2 px-5 border-amber-300 rounded'}>Contact</li>
+			</ul>
+			<button className={'bg-amber-300 px-6 font-secondary'}>Login</button>
 		</div>
 	);
 }
