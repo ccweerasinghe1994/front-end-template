2eb7c35b82584e59f034386498f8f5342061b31f
ccweerasinghe1994 <chamara.weerasinghe@my.sliit.lk>
2023-10-09 19:52:10 +0530

    feat: adding redux toolkit

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -1,5 +1,15 @@
+import { useAppDispatch, useAppSelector } from '../app/hooks.ts';
+import { increment } from '../features/counter/counterSlice.ts';
+
 const HomePage = () => {
-	return <div>Home Page</div>;
+	const dispatch = useAppDispatch();
+	const value = useAppSelector((state) => state.counter.value);
+	return (
+		<div>
+			<h1>{value}</h1>
+			<button onClick={() => dispatch(increment())}>count up</button>
+		</div>
+	);
 };
 
 export default HomePage;
